427b7113a1ae87601bf6f46c9231a6f1ab11c408
Mateusz Wojtas <103265505+Matecek@users.noreply.github.com>
2023-11-26 13:45:36 +0100

    Module 2 - Renderowanie i stan

---
+++ Module 2/Lesson 6 - Formularze/src/App.jsx
@@ -0,0 +1,19 @@
+import { Form } from "./components/Form";
+import { LikesCounter } from "./components/LikesCounter";
+import { Plot } from "./components/Plot";
+
+function App() {
+  console.log("<App> - render");
+
+  return (
+    <>
+      <h1>Gwiezdne wojny V</h1>
+      <h2>Rok produkcji: 1980</h2>
+      <LikesCounter />
+      <Plot />
+      <Form />
+    </>
+  );
+}
+
+export default App;
