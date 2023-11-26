7daffad9d40a88ce9c21cf086b29525bd8471e60
Mateusz Wojtas <103265505+Matecek@users.noreply.github.com>
2023-11-26 11:34:23 +0100

    Module 2 - Renderowanie i stan

---
+++ Module 2/Lesson 5 - Grupowanie logiki/src/App.jsx
@@ -0,0 +1,17 @@
+import { LikesCounter } from "./components/LikesCounter";
+import { Plot } from "./components/Plot";
+
+function App() {
+  console.log("<App> - render");
+
+  return (
+    <>
+      <h1>Gwiezdne wojny V</h1>
+      <h2>Rok produkcji: 1980</h2>
+      <LikesCounter />
+      <Plot />
+    </>
+  );
+}
+
+export default App;
