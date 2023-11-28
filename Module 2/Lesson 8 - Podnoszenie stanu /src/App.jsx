f14fe045b5b643e5061958fbe68f2fd692c8488e
Mateusz Wojtas <103265505+Matecek@users.noreply.github.com>
2023-11-28 12:00:10 +0100

    Module 2 - Renderowanie i stan

---
+++ Module 2/Lesson 8 - Podnoszenie stanu /src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { Form } from "./components/Form";
+import { LikesCounter } from "./components/LikesCounter";
+import { Plot } from "./components/Plot";
+import { Reviews } from "./components/Reviews";
+
+const initialRewiews = [
+    { author: "Brian", text: "Najlepszy film ever!", id: 1 },
+    { author: "Jessica", text: "Nie podobał mi się", id: 2 },
+];
+
+function App() {
+    const [reviews, setReviews] = useState(initialRewiews);
+
+    return (
+        <>
+            <h1>Gwiezdne wojny V</h1>
+            <h2>Rok produkcji: 1980</h2>
+            <LikesCounter />
+            <Plot />
+            <Reviews reviews={reviews} />
+            <Form
+                onReviewSubmit={(author, text) => {
+                    setReviews((prevReviews) => {
+                        return [
+                            { author, text, id: prevReviews.length + 1 },
+                            ...prevReviews,
+                        ];
+                    });
+                }}
+            />
+        </>
+    );
+}
+
+export default App;
