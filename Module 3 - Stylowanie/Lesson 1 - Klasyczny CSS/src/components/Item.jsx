83cfa7213d689e17d22ba3f0b86fe95e9cc72267
Mateusz Wojtas <103265505+Matecek@users.noreply.github.com>
2023-11-28 12:45:57 +0100

    Module 3 - Stylowanie

---
+++ Module 3 - Stylowanie/Lesson 1 - Klasyczny CSS/src/components/Item.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+export function Item({ emoji }) {
+    const [zoomed, setZoomed] = useState(false);
+
+    return (
+        <li className="item">
+            <span className={`emoji ${zoomed ? "zoomed" : ""}`}>{emoji}</span>
+            <button
+                className="btn"
+                onClick={() => {
+                    setZoomed((wasZoomed) => !wasZoomed);
+                }}
+            >
+                {zoomed ? "Oddal" : "Przybliż"}
+            </button>
+        </li>
+    );
+}
