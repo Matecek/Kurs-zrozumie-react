44d46b204e7eb975390e7c344c4a662330028303
Mateusz Wojtas <103265505+Matecek@users.noreply.github.com>
2023-11-28 12:22:30 +0100

    Module 2 - Renderowanie i stan

---
+++ Module 2/Lesson 9 - Niszczenie stanu/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { Form } from "./components/Form";
+import { LikesCounter } from "./components/LikesCounter";
+import { Plot } from "./components/Plot";
+import { Reviews } from "./components/Reviews";
+
+const initialRewiews = [
+    { author: "Brian", text: "Najlepszy film ever!", id: 1 },
+    { author: "Jessica", text: "Nie podobał mi się", id: 2 },
+];
+
+function App() {
+    const [reviews, setReviews] = useState(initialRewiews);
+    const [counterShown, setCounterShwon] = useState(true);
+    const [numberOfLikes, setNumberOfLikes] = useState(50);
+
+    function handleLikeButtonClick() {
+        setNumberOfLikes((previousNumberOfLikes) => previousNumberOfLikes + 1);
+    }
+
+    function handleLoveButtonClick() {
+        setNumberOfLikes((previousNumberOfLikes) => previousNumberOfLikes + 3);
+    }
+
+    return (
+        <>
+            <h1>Gwiezdne wojny V</h1>
+            <h2>Rok produkcji: 1980</h2>
+            <button
+                onClick={() => {
+                    setCounterShwon((prevCounterShwon) => !prevCounterShwon);
+                }}
+            >
+                {counterShown ? "Schowaj counter" : "Pokaż counter"}
+            </button>
+            {counterShown && (
+                <LikesCounter
+                    numberOfLikes={numberOfLikes}
+                    onLikeButtonClick={handleLikeButtonClick}
+                    onLoveButtonClick={handleLoveButtonClick}
+                />
+            )}
+            <Plot />
+            <Reviews reviews={reviews} />
+            <Form
+                onReviewSubmit={(author, text) => {
+                    setReviews((prevReviews) => {
+                        return [
+                            { author, text, id: prevReviews.length + 1 },
+                            ...prevReviews,
+                        ];
+                    });
+                }}
+            />
+        </>
+    );
+}
+
+export default App;
---
+++ Module 2/Lesson 9 - Niszczenie stanu/src/components/Form.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+export function Form({ onReviewSubmit }) {
+    const [inputValue, setInputValue] = useState("");
+    const [textareaValue, setTextareaValue] = useState("");
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const author = inputValue;
+        const text = textareaValue;
+
+        onReviewSubmit(author, text);
+
+        setInputValue("");
+        setTextareaValue("");
+    }
+
+    console.log("Wartość zmiennej stanowej input: ", inputValue);
+    console.log("Wartość zmiennej stanowej textarea: ", textareaValue);
+    return (
+        <>
+            <hr />
+
+            <h2>Dodaj recenzję</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <div>
+                        <label htmlFor="author">Autor</label>
+                    </div>
+                    <input
+                        type="text"
+                        name="author"
+                        id="author"
+                        value={inputValue}
+                        onChange={(event) => {
+                            setInputValue(event.target.value);
+                        }}
+                    />
+                </div>
+                <div>
+                    <div>
+                        <label htmlFor="text">Tekst</label>
+                    </div>
+                    <textarea
+                        name="text"
+                        id="text"
+                        value={textareaValue}
+                        onChange={(event) => {
+                            setTextareaValue(event.target.value);
+                        }}
+                    ></textarea>
+                </div>
+                <button
+                    type="submit"
+                    disabled={inputValue === "" || textareaValue === ""}
+                >
+                    Dodaj
+                </button>
+            </form>
+        </>
+    );
+}
