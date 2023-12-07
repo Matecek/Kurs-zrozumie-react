cf3cf6ccbdc77ec3a319f6199a6dbb280e589dc0
Mateusz Wojtas <103265505+Matecek@users.noreply.github.com>
2023-12-07 13:21:40 +0100

    Module 3 - stylowanie

---
+++ Module 3 - Stylowanie/Lesson 4 - CSS w JS/src/components/List/List.jsx
@@ -0,0 +1,38 @@
+import { Item } from "../Item/Item";
+import styled from "styled-components";
+import { StyledButton } from "../styles/StyledButton";
+
+const emojis = ["🚀", "🎶", "🎁", "🎉", "✨"];
+
+const StyledList = styled.ul`
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    gap: 30px;
+    padding: 30px;
+`;
+
+const StyledListItem = styled(Item)`
+    margin-bottom: 50px;
+`;
+
+export function List() {
+    return (
+        <>
+            <StyledButton
+                $primary
+                $margin={30}
+                onClick={() => {
+                    alert(`Liczba emoji: ${emojis.length}`);
+                }}
+            >
+                Pokaż liczbe emoji
+            </StyledButton>
+            <StyledList>
+                {emojis.map((emoji) => (
+                    <StyledListItem key={emoji} emoji={emoji} />
+                ))}
+            </StyledList>
+        </>
+    );
+}
