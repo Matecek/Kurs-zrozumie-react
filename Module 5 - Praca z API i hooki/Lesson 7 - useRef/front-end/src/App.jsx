4051c7cf037d86c10f74901b982b930b24ae7b5f
Mateusz Wojtas <103265505+Matecek@users.noreply.github.com>
2024-01-12 22:05:29 +0100

    Module 5 - Praca z API i hooki

---
+++ Module 5 - Praca z API i hooki/Lesson 7 - useRef/front-end/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState, useCallback } from "react";
+import styles from "./App.module.css";
+import { Panel } from "./components/Panel/Panel";
+import { Button } from "./components/Button/Button";
+import { ErrorMessage } from "./components/ErrorMessage/ErrorMessage";
+import { Timer } from "./components/Timer/Timer";
+
+function App() {
+    const [isPanelShown, setIsPanelShown] = useState(true);
+    const [error, setError] = useState(null);
+
+    const handleError = useCallback((e) => {
+        setError(e.message);
+        setTimeout(() => {
+            setError(null);
+        }, 3000);
+    }, []);
+
+    return (
+        <main className={styles.main}>
+            {error && <ErrorMessage>{error}</ErrorMessage>}
+            <Button
+                onClick={() => {
+                    setIsPanelShown((prevIsPanelShown) => !prevIsPanelShown);
+                }}
+            >
+                {isPanelShown ? "Schowaj panel" : "Pokaż panel"}
+            </Button>
+            {isPanelShown && <Panel onError={handleError} />}
+            <Timer />
+        </main>
+    );
+}
+
+export default App;
---
+++ Module 5 - Praca z API i hooki/Lesson 7 - useRef/front-end/src/components/Form/Form.jsx
@@ -0,0 +1,75 @@
+import { useState, useRef, forwardRef } from "react";
+import styles from "./Form.module.css";
+import { Button } from "../Button/Button";
+
+const Input = forwardRef(function Input(props, ref) {
+    return <input ref={ref} type="text" id="word" className={styles.input} />;
+});
+
+export function Form({ onFormSubmit }) {
+    const [translation, setTranslation] = useState("");
+    const [category, setCategory] = useState("noun");
+    const wordInputRef = useRef(null);
+
+    function handleSubmit(e) {
+        console.log(wordInputRef);
+        e.preventDefault();
+        const newItem = {
+            word: wordInputRef.current.value,
+            translation,
+            category,
+        };
+
+        onFormSubmit(newItem);
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className={styles.form}>
+            <div className={styles.row}>
+                <div className={styles.cell}>
+                    <label htmlFor="word">Słowo</label>
+                    <Input
+                        ref={wordInputRef}
+                        type="text"
+                        id="word"
+                        className={styles.input}
+                    />
+                </div>
+                <div className={styles.cell}>
+                    <label htmlFor="translation">Tłumaczenie</label>
+                    <input
+                        type="text"
+                        id="translation"
+                        className={styles.input}
+                        value={translation}
+                        onChange={(e) => setTranslation(e.target.value)}
+                    />
+                </div>
+            </div>
+            <div>Wybierz kategorię:</div>
+            <div className={styles.category}>
+                <input
+                    type="radio"
+                    name="category"
+                    id="category-noun"
+                    checked={category === "noun"}
+                    onChange={() => setCategory("noun")}
+                />
+                <label htmlFor="category-noun">Rzeczownik</label>
+            </div>
+            <div className={styles.category}>
+                <input
+                    type="radio"
+                    name="category"
+                    id="category-verb"
+                    checked={category === "verb"}
+                    onChange={() => setCategory("verb")}
+                />
+                <label htmlFor="category-verb">Czasownik</label>
+            </div>
+            <div>
+                <Button>Dodaj</Button>
+            </div>
+        </form>
+    );
+}
