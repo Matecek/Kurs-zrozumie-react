0c8576bcf9364337a1efe83d07e027d4e877782a
Mateusz Wojtas <103265505+Matecek@users.noreply.github.com>
2024-01-14 13:16:28 +0100

    Module 6 - Stan, ale trudniejszy

---
+++ Module 6 - Stan, ale trudniejszy/Lesson 3 - useContext + useState/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import styles from "./App.module.css";
+import { recipes } from "./data/recipes";
+import { List } from "./components/List/List";
+import { Cookbook } from "./components/Cookbook/Cookbook";
+import { Recipe } from "./components/Recipe/Recipe";
+import { TopBar } from "./components/TopBar/TopBar";
+import { RecipeContext } from "./context/RecipeContext";
+import { IsLoggedInStateContext } from "./context/IsLoggedInStateContext";
+
+function App() {
+    const [selectedRecipeId, setSelectedRecipeId] = useState(1);
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+    const selectedRecipe = recipes.find(
+        (recipe) => recipe.id === selectedRecipeId
+    );
+
+    return (
+        <IsLoggedInStateContext.Provider value={[isLoggedIn, setIsLoggedIn]}>
+            <TopBar />
+            <div className={styles.container}>
+                <RecipeContext.Provider value={selectedRecipe}>
+                    <List
+                        recipes={recipes}
+                        onSelectRecipe={(id) => setSelectedRecipeId(id)}
+                    />
+                    <Cookbook heading="Książka kucharska" />
+                </RecipeContext.Provider>
+            </div>
+
+            <div className={styles.promoted}>
+                <h2>Przepis tygoodnia:</h2>
+                <RecipeContext.Provider value={recipes[3]}>
+                    <Recipe />
+                </RecipeContext.Provider>
+            </div>
+        </IsLoggedInStateContext.Provider>
+    );
+}
+
+export default App;
---
+++ Module 6 - Stan, ale trudniejszy/Lesson 3 - useContext + useState/src/components/Article/Article.jsx
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import { Recipe } from "../Recipe/Recipe";
+import { Button } from "../Button/Button";
+import { IsLoggedInStateContext } from "../../context/IsLoggedInStateContext";
+
+export function Article() {
+    const [isLoggedIn, setIsLoggedIn] = useContext(IsLoggedInStateContext);
+    return (
+        <article>
+            <Recipe />
+            {isLoggedIn ? (
+                <Button onClick={() => alert("Przepis polubiony!")}>
+                    Lubię to!
+                </Button>
+            ) : (
+                <>
+                    <div>Zaloguj się, aby polubić przepis:</div>
+                    <Button onClick={() => setIsLoggedIn(true)}>
+                        Zaloguj się
+                    </Button>
+                </>
+            )}
+        </article>
+    );
+}
