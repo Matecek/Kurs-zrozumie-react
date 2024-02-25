89e228449db76be5635b9eee9a91b272fdc50157
Mateusz Wojtas <103265505+Matecek@users.noreply.github.com>
2024-02-25 16:19:35 +0100

    Module 6 - Stan, ale trudniejszy

---
+++ Module 6 - Stan, ale trudniejszy/Lesson 5 - Custom Hooki/src/components/Employees/Employees.jsx
@@ -0,0 +1,35 @@
+import styles from "./Employees.module.css";
+import { useGetData } from "../../hooks/useGetData";
+
+export function Employees() {
+    const { data: employees, error } = useGetData("/employees.json");
+
+    return (
+        <section className={styles.section}>
+            {error ? (
+                error.message
+            ) : (
+                <table className={styles.tableContainer}>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Salary</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {employees.map((employee) => (
+                            <tr key={employee.id}>
+                                <td>{employee.id}</td>
+                                <td>
+                                    {employee.name} {employee.last_name}
+                                </td>
+                                <td>{employee.salary}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
+        </section>
+    );
+}
