a1d2e404b3d3885c52bf4cb931ceec05a89d9bdc
Mateusz Wojtas <103265505+Matecek@users.noreply.github.com>
2024-03-10 12:48:15 +0100

    Module 6 - Stan, ale trudniejszy

---
+++ Module 6 - Stan, ale trudniejszy/Lesson 6 - Portal/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Main } from "./components/Main/Main";
+
+function App() {
+    return (
+        <>
+            <h1>Witaj w lekcji o React Portal!</h1>
+            <Main />
+            <footer>zrozumiecReact.pl ©</footer>
+        </>
+    );
+}
+
+export default App;
